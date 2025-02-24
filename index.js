bb36e2c0c73d0a7671ae0ffbb7ca8cf3c5727379
Brian <okellobrian18@gmail.com>
2025-02-24 15:18:22 +0300

    Loops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(name) {
+    const thankYou  = [];
+
+    for(let count = 0; count < name.length; count+=1) {
+        thankYou.push(`Thank you, ${name[count]}, for the wonderful surprise gift!`)
+    }
+    return thankYou;
+}
+
+function countDown(num) {
+    for(let count = num; count > -1; count--) {
+        console.log(count)
+    }
+}
